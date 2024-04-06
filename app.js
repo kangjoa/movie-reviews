b05c4a47d7b755068c5ed714f61a0165b389155b
kangjoa <joannalaikang@gmail.com>
2024-04-06 12:56:47 -0700

    Users can see single reviews

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 const { engine } = require('express-handlebars');
+const Handlebars = require('handlebars');
+const allowPrototypeAccess =
+  require('@handlebars/allow-prototype-access').allowInsecurePrototypeAccess;
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/movie-reviews', {
-  useNewUrlParser: true,
-});
-// mongoose.connect('mongodb://localhost/movie-reviews');
+// mongoose.connect('mongodb://localhost/movie-reviews', {
+//   useNewUrlParser: true,
+// });
+
+mongoose.connect('mongodb://localhost/movie-reviews');
 
 const Review = mongoose.model('Review', {
   title: String,
@@ -17,9 +21,14 @@ const Review = mongoose.model('Review', {
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.engine(
   'handlebars',
-  engine({ extname: '.handlebars', defaultLayout: 'main' }),
+  engine({
+    extname: '.handlebars',
+    defaultLayout: 'main',
+    handlebars: allowPrototypeAccess(Handlebars),
+  }),
 );
 app.set('view engine', 'handlebars');
 
@@ -50,6 +59,29 @@ app.post('/reviews', (req, res) => {
     .then((review) => {
       console.log(review);
       res.redirect('/');
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+});
+
+// SHOW
+app.get('/reviews/:id', (req, res) => {
+  Review.findById(req.params.id)
+    .then((review) => {
+      res.render('reviews-show', { review: review });
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+});
+
+// CREATE
+app.post('/reviews', (req, res) => {
+  Review.create(req.body)
+    .then((review) => {
+      console.log(review);
+      res.redirect(`/reviews/${review._id}`); // Redirect to reviews/:id
     })
     .catch((err) => {
       console.log(err.message);
