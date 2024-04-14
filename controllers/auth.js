b5c018262b02a12a6d59c59351a7fb5c4e6138a6
kangjoa <joannalaikang@gmail.com>
2024-04-13 20:29:24 -0700

    Users can sign up

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,22 +5,20 @@ module.exports = (app) => {
   // SIGN UP FORM
   app.get('/sign-up', (req, res) => res.render('sign-up'));
 
-  app.post('/sign-up', (req, res) => {
+  app.post('/sign-up', async (req, res) => {
     // Create User and JWT
     const user = new User(req.body);
 
-    user
-      .save()
-      .then(() => {
-        const token = jwt.sign({ _id: user._id }, process.env.SECRET, {
-          expiresIn: '60 days',
-        });
-        res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
-        return res.redirect('/');
-      })
-      .catch((err) => {
-        console.log(err.message);
-        return res.status(400).send({ err });
+    try {
+      await user.save();
+      const token = jwt.sign({ _id: user._id }, process.env.SECRET, {
+        expiresIn: '60 days',
       });
+      res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
+      return res.redirect('/');
+    } catch (err) {
+      console.log(err.message);
+      return res.status(400).send({ err });
+    }
   });
 };
