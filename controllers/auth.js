3ed1a6a0b6be761e08522d506067731d1a847831
kangjoa <joannalaikang@gmail.com>
2024-04-13 20:56:43 -0700

    Users can log in

--- controllers/auth.js
+++ controllers/auth.js
@@ -32,40 +32,39 @@ module.exports = (app) => {
   app.get('/login', (req, res) => res.render('login'));
 
   // LOGIN
-  app.post('/login', (req, res) => {
+  app.post('/login', async (req, res) => {
     const { username, password } = req.body;
-    // Find this user name
-    User.findOne({ username }, 'username password')
-      .then((user) => {
-        if (!user) {
-          // User not found
-          return res
-            .status(401)
-            .send({ message: 'Wrong Username or Password' });
-        }
-        // Check the password
-        user.comparePassword(password, (err, isMatch) => {
-          if (!isMatch) {
-            // Password does not match
-            return res
-              .status(401)
-              .send({ message: 'Wrong Username or password' });
-          }
-          // Create a token
-          const token = jwt.sign(
-            { _id: user._id, username: user.username },
-            process.env.SECRET,
-            {
-              expiresIn: '60 days',
-            },
-          );
-          // Set a cookie and redirect to root
-          res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
-          return res.redirect('/');
+
+    try {
+      const user = await User.findOne({ username }, 'username password');
+      if (!user) {
+        // User not found
+        return res.status(401).send({ message: 'Wrong Username or Password' });
+      }
+
+      // Check the password
+      const isMatch = await new Promise((resolve, reject) => {
+        user.comparePassword(password, (err, match) => {
+          if (err) reject(err);
+          resolve(match);
         });
-      })
-      .catch((err) => {
-        console.log(err);
       });
+
+      // Create a token
+      const token = jwt.sign(
+        { _id: user._id, username: user.username },
+        process.env.SECRET,
+        {
+          expiresIn: '60 days',
+        },
+      );
+
+      // Set a cookie and redirect to root
+      res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
+      return res.redirect('/');
+    } catch (err) {
+      console.log(err);
+      res.status(500).send({ message: 'Internal Server Error' });
+    }
   });
 };
